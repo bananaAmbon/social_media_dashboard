8c4d889707e0ce492d22d53e7a2bc3740b399451
bananaAmbon <singgih.ariwibowo22@gmail.com>
2019-08-08 08:40:28 +0700

    using axios to request

---
+++ src/Api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const BASEURL = 'http://jsonplaceholder.typicode.com';
+
+export const api = {
+  get
+}
+
+async function get(endPoint){
+  try {
+    const response = await axios.get(BASEURL + endPoint, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    return response;
+  } catch (error) {
+    return Promise.reject(error.response);
+  }  
+}
